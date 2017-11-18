b8bd6f70375d00c88d3da153843ee541d3cb0706
Alexis A <alexis.amiand@gmail.com>
2017-11-18 16:44:02 +0100

    refactor navbar

---
+++ rentingMachineJava/web/inc/header.php
@@ -0,0 +1,24 @@
+        <div class="text-center p-5">
+            <div class="container-fluid text-center">
+                <div class="row">
+                    <div class="col-md-12 align-self-center col-12 col-sm-12 col-lg-12 col-xl-12 text-center">
+                        <ul class="nav nav nav-pills text-center text-capitalize 
+                            align-items-center
+                            d-flex justify-content-center">
+                            <li class="nav-item">
+                                <a href="#" class="active nav-link"><i class="fa fa-home fa-home"></i>Accueil</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Réservations</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Clients</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Chambres</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
